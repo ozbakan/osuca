b8f0904df60422bf363bbc5d37583b427e37deb4
tolga <tolga@ozbakan.com>
2022-07-21 21:20:28 +0300

    Test commit

--- osuca/__init__.py
+++ osuca/__init__.py
@@ -17,7 +17,7 @@ def create_app(test_config=None):
 
     if test_config is None:
         # Load the instance config, if it exists, when not testing.
-        # If file is not found,  we will carry on with default configuration.
+        # If file is not found, we will carry on with default configuration.
         app.config.from_pyfile('config.py', silent=True)
     else:
         # Load the test config if passed in
