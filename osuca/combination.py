1cc876bf0ec85513bbc3cda771ca5b6e3f08ee6d
tolga <tolga@ozbakan.com>
2022-08-03 12:23:41 +0300

    Integration

--- osuca/combination.py
+++ osuca/combination.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template, request
-from werkzeug.exceptions import abort
 
 from osuca.db import get_db
 
--- osuca/summary.py
+++ osuca/summary.py
@@ -1,6 +1,7 @@
+from re import M
 from webbrowser import get
 import requests
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 from osuca.db import get_db
 
@@ -9,16 +10,22 @@ bp = Blueprint('summary', __name__)
 
 @bp.route("/fetch", methods=['GET', 'POST'])
 def fetch():
-    db = get_db()
 
     microservice_result = None
     try:
         response = requests.get('http://localhost:3000/stats').json()
-        stats = response['allCourseStatistics']
-        microservice_result = "Getting some where..."
+        microservice_result = response['allCourseStatistics']
     except:
-        microservice_result = "Could not access summary service."
+        flash("Could not access summary service.")
+
+    db = get_db()
+    selection_label = "All Courses"
+    if request.method == 'POST' and request.form['course'] != "All Courses":
+        selection_label = request.form['course']
+        selection = db.course(selection_label)
+        microservice_result = db.course_combination_aggregate(selection)
 
     return render_template("summary.html",
+                           selection_label=selection_label,
                            course=sorted(db.course()),
                            microservice_result=microservice_result)
