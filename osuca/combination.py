f67fb192ccbda56059f1e87e6c0e31c5da0ba822
tolga <tolga@ozbakan.com>
2022-08-04 07:25:40 +0300

    Add row limit

--- osuca/combination.py
+++ osuca/combination.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, current_app
 
 from osuca.db import get_db
 
@@ -20,10 +20,14 @@ def combination(key, reverse):
     def count(cca): return cca[1].count
     def mean(cca): return cca[1].mean
     dispatcher = {'course': course, 'count': count, 'mean': mean}
+    row_limit = None
+    if 'ROW_LIMIT' in current_app.config:
+      row_limit = current_app.config['ROW_LIMIT']
 
     return render_template("combination.html",
                            selection_label=selection_label,
                            course=sorted(db.course()),
+                           row_limit=row_limit,
                            query_result=sorted(query_result,
                                                key=dispatcher[key],
                                                reverse=(reverse == 'True')))
